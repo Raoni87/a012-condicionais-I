3a1044f500a5bf941a437171adeb720c7f9b80df
Raoni87 <raonioliva@gmail.com>
2022-08-03 22:36:35 -0300

    resolucao exercicios a012-condicionais

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 35;
+let isMaiorIdade = true;
+let isCursandoOutraFaculdade = false;
 
-if(){
-
+if(idade >= 18){
+    console.log('a pessoa é maior de idade')
 }else{
-	
+	console.log('a pessoa é menor de idade')
+}
+
+if(isMaiorIdade = true) {
+    console.log('a pessoa é maior de idade')
+} else {
+    console.log('a pessoa é menor de idade')
+}
+
+if(isCursandoOutraFaculdade = true) {
+    console.log('a pessoa está cursando outra faculdade')
+} else {
+
 }
